8adf52fbbac0f3f4561a524807d61b9159abf6bf
IuriiOnopko <marnilyp@icloud.com>
2025-02-03 20:10:47 -0800

    Implement user authentication in navbar with SignIn component; add styles for sign-in button.

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,14 +1,34 @@
+'use client';
+
 import styles from "./navbar.module.css"
 import Image from "next/image";
 import Link from "next/link";
+import SignIn from "./sign-in";
+import { useEffect, useState } from "react";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { User } from "firebase/auth";
 
 export default function Navbar(){
+
+  const [user, setUser] = useState<User | null>(null);
+  
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+    }, []);
+    
+  
+
   return(
     <nav className={styles.nav}>
         <Link href="/">
             <Image width={90} height={20} 
               src="/youtube-logo.svg" alt="YouTube logo"/>
         </Link>
+        <SignIn user={user} />
     </nav>      
   );
 }
